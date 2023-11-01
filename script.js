07d7d89a6edd904a81998d03e41ac6abe9b6d2c7
stanciudrg <stanciudragos1337@gmail.com>
2023-11-01 13:55:16 +0200

    Create Players module in JavaScript
    The Players module creates two player objects within itself by using
    a private constructor function, and sets their corresponding initial
    values. Players have a type (Player, Computer), a difficulty level
    (easy, medium, unbeatable), a value (x, zero), and a score.
    The Player module returns two methods:
    
    getPlayerOne() and getPlayerTwo()

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ const Panel = (function () {
     return { createPanel, addValue, getPanel };
 
 })();
+
+const Players = (function () {
+
+    const playerOne = createPlayer();
+    playerOne.setType("Player");
+    playerOne.setValue("x");
+
+    const playerTwo = createPlayer();
+    playerTwo.setType("Player");
+    playerTwo.setValue("zero");
+
+    function createPlayer() {
+
+        let type;
+        const setType = (newType) => type = newType;
+        const getType = () => type;
+
+        let difficulty;
+        const setDifficulty = (newDifficulty) => difficulty = newDifficulty;
+        const getDifficulty = () => difficulty
+
+        let value;
+        const setValue = (newValue) => value = newValue;
+        const getValue = () => value;
+
+        let score = 0;
+        const getScore = () => score > 0 ? score : "-";
+        const addScore = () => score++;
+        const resetScore = () => score = 0;
+
+        return { setType, getType, setDifficulty, getDifficulty, getValue, setValue, addScore, getScore, resetScore };
+
+    }
+
+    const getPlayerOne = () => playerOne;
+    const getPlayerTwo = () => playerTwo;
+
+    return { getPlayerOne, getPlayerTwo }
+
+})();
