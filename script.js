36a70501489c0a1b7e97356be678cb2f8f268b74
stanciudrg <stanciudragos1337@gmail.com>
2023-11-01 14:15:11 +0200

    Create Renderer module in JavaScript
    The Renderer module collects all DOM elements and returns
    an object with the following methods:
    
    renderPanel => loops through Panel's array and renders a square inside game container for each array element
    
    refreshPanel => refreshes the renderedPanel by removing the 'x' and
    'zero' classes which will be added by the InputHandler module
    
    displayOpponentType && displayOpponentDifficulty => reflects the
    difficulty and type of Players.playerTwo object into the
    changeOpponent DOM button
    
    displayValue => adds the value of the current player into the
    specified square by adding a class value of either 'x' or 'zero'
    
    displayTurn => reflects the current player by changing the color of one of the two elements positioned above the game container, which contain each player's score and value (x or zero);
    
    hideTurn => removes the color that denotes the current player in
    case the game is a Tie
    
    updateScore => updates players score on the page
    
    displayWinner && hideWinner => display or hide the winner by adding or removing  the .winner classList to the respective player
    
    displayWinningPath && removeWinningPath => emphasize the square combination that resulted in a win and hide it when the game is restarted

--- script.js
+++ script.js
@@ -63,3 +63,119 @@ const Players = (function () {
     return { getPlayerOne, getPlayerTwo }
 
 })();
+
+const Renderer = (function () {
+
+    const opponentButton = document.querySelector('.change-opponent');
+    const resetButton = document.querySelector('.reset-score');
+    const playAgainButton = document.querySelector('.play-again');
+    const panelContainer = document.querySelector('.panel-container');
+    const squares = panelContainer.getElementsByClassName('square');
+
+    const getOpponentButton = () => opponentButton;
+    const getResetButton = () => resetButton;
+    const getPlayAgainButton = () => playAgainButton;
+    const getPanelContainer = () => panelContainer;
+    const getSquares = () => squares;
+
+
+    const renderPanel = () => {
+
+        panelContainer.textContent = "";
+
+        Panel.getPanel().forEach((value, index) => {
+
+            const square = document.createElement("button");
+            square.dataset.index = index;
+            square.classList.add("square");
+            panelContainer.appendChild(square);
+
+        })
+
+    };
+
+    const refreshPanel = () => {
+
+        for (let square of squares) {
+
+            square.classList.remove('x');
+            square.classList.remove('zero');
+            square.textContent = "";
+
+        }
+
+    }
+
+    const displayOpponentType = (opponentType) => {
+
+        opponentButton.querySelector('.opponent-type').textContent = opponentType;
+
+    }
+
+    const displayOpponentDifficulty = (opponentDifficulty) => {
+
+        opponentButton.querySelector('.opponent-difficulty').textContent = opponentDifficulty;
+
+    }
+
+    const displayValue = (index, currentPlayer) => {
+
+        squares[index].classList.add(currentPlayer.getValue());
+        squares[index].textContent = ".";
+
+    }
+
+
+    const displayTurn = (currentPlayer) => {
+
+        document.getElementById(currentPlayer.getValue()).classList.add('show');
+        document.getElementById(currentPlayer.getValue() == "x" ? "zero" : "x").classList.remove('show');
+
+    }
+
+    const hideTurn = (currentPlayer) => {
+
+        document.getElementById(currentPlayer.getValue()).classList.remove('show');
+
+    }
+
+    const updateScore = (playerOne, playerTwo) => {
+
+        document.getElementById(playerOne.getValue()).lastElementChild.textContent = playerOne.getScore();
+        document.getElementById(playerTwo.getValue()).firstElementChild.textContent = playerTwo.getScore();
+
+    }
+
+    const displayWinner = (winner) => {
+
+        document.getElementById(winner.getValue()).classList.add('winner');
+
+    }
+
+    const hideWinner = (winner) => {
+
+        document.getElementById(winner.getValue()).classList.remove('winner');
+
+    }
+
+    const displayWinningPath = (first, second, third) => {
+
+        squares[first].classList.add('winner');
+        squares[second].classList.add('winner');
+        squares[third].classList.add('winner');
+
+    }
+
+    const removeWinningPath = () => {
+
+        for (let square of squares) {
+
+            square.classList.remove('winner');
+
+        }
+
+    }
+
+    return { getPanelContainer, displayOpponentType, displayOpponentDifficulty, getSquares, getOpponentButton, hideTurn, getResetButton, getPlayAgainButton, renderPanel, refreshPanel, updateScore, displayValue, displayTurn, displayWinner, hideWinner, displayWinningPath, removeWinningPath };
+
+})();
